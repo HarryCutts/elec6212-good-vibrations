ef9bf4510f21a4837a8444f408385dd6475a61cc
Samuel Bennett <samuelbennett93@gmail.com>
2015-04-22 10:20:58 +0100

    First working version
    Needs tweaking of the sample rate and the threasholding

--- receiver.py
+++ receiver.py
@@ -8,131 +8,38 @@ import time
 
 BAUD_RATE = 1000000
 
-MEASUREMENT_SIZE = 2
-
-HOST_READY = 'r'
-HEADER_DATA_SIZE = ord('s')
-BUFFER_START = ord('b')
-
 port = sys.argv[1]
 
-def send_ready(ser):
-    ser.write(bytes(HOST_READY, 'ascii'))
-
-def read_data_size_header(ser):
-    data = bytes(ser.read(size=4))
-    assert data[0] == HEADER_DATA_SIZE
-
-    measurements_per_buffer = data[1] | data[2] << 8
-    num_inputs = data[3]
-
-    return num_inputs, measurements_per_buffer
-
-def read_block(ser, block_size):
-    print("Waiting for block header...")
-    header = bytes(ser.read(size=9+block_size[0]))
-    assert header[0] == BUFFER_START
-    # (Throw away the times)
-    order = (header[9], header[10], header[11], header[12])
-
-    num_bytes_to_read = block_size[0] * block_size[1] * MEASUREMENT_SIZE
-    print("Waiting for block body...")
-    data = bytes(ser.read(size=num_bytes_to_read))
-    return (data, order)
-
 ser = serial.Serial(baudrate=BAUD_RATE)
+
+totaldata = [[0] for i in range(4)]
+
+mic = 0
 
 try:
     ser.setPort(port)
     ser.open()
 
-    send_ready(ser)
-    block_size = read_data_size_header(ser)
-    print("Number of inputs and buffer size:", block_size)
+    for block_number in range(5):
+        print("Next Block")
+        for y in range(2048*4*4):
+            while ser.inWaiting() == 0:
+                pass
 
-    time.sleep(0.1)
+            d = ser.read(size=2)
+            num = d[0] | d[1] << 8
+            print(str(mic) + " -> " + str(num))
 
-    d = np.array([])
-    while True:
-        while ser.inWaiting() == 0:
-            pass
+            totaldata[mic].append(num)
 
-        block = read_block(ser, block_size)
-        print("got")
-        data_array = np.fromstring(block[0], dtype='<i2').reshape(block_size[1],block_size[0]).swapaxes(0,1)
-            
-        plt.clf()
-
-        plt.plot(data_array.swapaxes(0,1));
-        plt.show();
-
-        means = data_array.mean(axis=1)
-        rangeR = data_array.ptp(axis=1)
-
-        # print(means)
-        print(block[1])
-        # print(means[:, np.newaxis])
-
-        meanC = data_array - means[:, np.newaxis];
-        norm = meanC / rangeR[:, np.newaxis];
-
-        d = np.diff(norm, axis=1)
-        a = np.absolute(d);
-
-        index = 0;
-        for x in a[0]:
-            if x>0.1:
-                print(index);
-                break;
-            index = index + 1;
-
-        index1 = 0;
-        for x in a[1]:
-            if x>0.1:
-                print(index1);
-                break;
-            index1 = index1 + 1;
-
-        index2 = 0;
-        for x in a[2]:
-            if x>0.1:
-                print(index2);
-                break;
-            index2 = index2 + 1;
-
-        index3 = 0;
-        for x in a[3]:
-            if x>0.10:
-                print(index3);
-                break;
-            index3 = index3 + 1;
-
-
-        start = min([index,index1,index2,index3]) - 100
-        print(start)
-
-        plt.clf()
-        plt.plot(a[0][start:]);
-        plt.plot(a[1][start:]);
-        plt.plot(a[2][start:]);
-        plt.plot(a[3][start:]);
-        plt.show();
-
-        for x in range(4):
-            for y in range(4):
-                if x!=y:
-                    corrData = plt.xcorr(data_array[x][start:],data_array[y][start:],normed=False);
-                    maxLag = corrData[0][np.argmax(corrData[1])]
-                    print(str(x) + " " + str(y) + " -> " + str(maxLag));
-
-        import math
-        first = plt.xcorr(data_array[3][start:],data_array[1][start:],normed=False);
-        firstmaxLag = first[0][np.argmax(first[1])]
-        second = plt.xcorr(data_array[2][start:],data_array[0][start:],normed=False);
-        secondmaxLag = second[0][np.argmax(second[1])]
-
-
-        print(math.atan2(firstmaxLag,secondmaxLag) * (180/math.pi))
+            mic = (mic + 1) % 4
+        #print(np.array(data).mean(axis=1))
 
 finally:
     ser.close()
+
+plt.plot(totaldata[0])
+plt.plot(totaldata[1])
+plt.plot(totaldata[2])
+plt.plot(totaldata[3])
+plt.show()
--- robot-code.ino
+++ robot-code.ino
@@ -1,165 +1,23 @@
-#include <math.h>
-
-#define SHOW_TIMINGS
-
-const unsigned long SAMPLE_RATE =    10000UL;
+const unsigned long SAMPLE_RATE =    100000UL;
 const unsigned long CLOCK_MAIN  = 84000000UL;
 #define TMR_CNTR (CLOCK_MAIN / (2 * SAMPLE_RATE))
 
 const uint16_t NUM_INPUTS = 4;
-const uint16_t microphone_averages[NUM_INPUTS]   = {1619, 1477, 1375, 1262}; //{1500, 1600, 1700, 1800};
-uint16_t microphone_averages_measured[NUM_INPUTS];
-const uint16_t microphone_thresholds[NUM_INPUTS] = {1900, 1800, 1700, 1600}; //{1600, 1700, 1800, 1900};
-const uint8_t THRESHOLD = 100;
 
-const uint16_t NUM_MIC_ID_AVERAGE_POINTS = 10;
-  // The number of data points to average when identifying microphones
-
-const uint8_t NUM_BUFFERS = 2;
-
-const uint16_t MEASUREMENTS_PER_BUFFER = 2048*2;
+const uint16_t MEASUREMENTS_PER_BUFFER = 2048*4;
   // The number of measurements *from each sensor* to store in the buffer
 const uint16_t INP_BUFF = MEASUREMENTS_PER_BUFFER * NUM_INPUTS;
 
-uint8_t mic_to_input_number[NUM_INPUTS];
-uint8_t input_to_mic_number[NUM_INPUTS];
-bool have_identified_microphones = false;
-
-typedef enum {
-  WAITING_FOR_CONNECTION,
-  SENDING_DATA
-} State;
-
-State state;
-
-typedef struct {
-  unsigned long start_time, end_time;
-    // The times at which the first and last measurements in the buffer
-    // were taken, in microseconds.
-
-  uint16_t data[INP_BUFF] = {0};     // DMA likes ping-pongs buffer
-} InputBuffer;
-
-void identify_microphones(InputBuffer &buff);
-void dump_buffer(InputBuffer &buff);
-void process_data(InputBuffer &buff);
-void serial_dump_buffer(InputBuffer &buff);
-bool check_buffer_for_tap(InputBuffer &buff);
-
-volatile bool data_to_process = false;
-volatile uint8_t write_buffer_index = 0, read_buffer_index = 0;
-
-InputBuffer buffers[NUM_BUFFERS];
-unsigned long data_start_time, data_end_time;
-  // The times at which the first and last measurements in the last buffer
-  // that was filled, in microseconds.
+uint16_t data[INP_BUFF] = {0};     // DMA likes ping-pongs buffer
 
 void setup() {
-  serial_init();
-  data_end_time = micros();
-  state = WAITING_FOR_CONNECTION;
+  SerialUSB.begin(10000000);  
+  adc_setup();
+  tmr_setup();
+  pio_TIOA0();  // drive Arduino pin 2 at SMPL_RATE to bring clock out
 }
-
-int mic_id_counter = 0;
 
 void loop() {
-  switch (state) {
-    case WAITING_FOR_CONNECTION:
-      if (serial_connection_ready()) {
-        adc_setup();
-        tmr_setup();
-        pio_TIOA0();  // drive Arduino pin 2 at SMPL_RATE to bring clock out
-        serial_send_size_header();
-        state = SENDING_DATA;
-      }
-      break;
-    case SENDING_DATA:
-      break;
-  }
-  if (data_to_process) {
-    unsigned long buffer_duration = data_end_time - data_start_time;
-    #ifdef SHOW_TIMINGS
-      Serial.println(buffer_duration);
-    #endif
-
-    InputBuffer &buff = buffers[read_buffer_index];
-    if (!have_identified_microphones) {
-      // The analogue inputs take a while to "warm up"
-      mic_id_counter++;
-      if (mic_id_counter >= 5){
-        identify_microphones(buff);
-        for (uint8_t mic_no = 0; mic_no < NUM_INPUTS; mic_no++) {
-          Serial.println(microphone_averages_measured[mic_no]);
-        }
-      }
-    }
-    else{
-      if (check_buffer_for_tap(buff)){
-        serial_dump_buffer(buff);
-      }
-    }
-
-    data_to_process = false;
-  }
-}
-
-void identify_microphones(InputBuffer &buff) {
-  for (uint8_t input = 0; input < NUM_INPUTS; input++) {
-    uint16_t sum = 0;
-    for (size_t i = input; i < NUM_MIC_ID_AVERAGE_POINTS * NUM_INPUTS; i += NUM_INPUTS) {
-      sum += buff.data[i];
-    }
-    uint16_t average = sum / NUM_MIC_ID_AVERAGE_POINTS;
-    
-    int mic_no = -1;
-    for (int i = 0; i < NUM_INPUTS; i++) {
-      uint16_t difference = abs(microphone_averages[i] - average);
-      if (difference < 50) {
-        mic_no = i;
-        break;
-      }
-    }
-    
-    mic_to_input_number[mic_no] = input;
-    input_to_mic_number[input] = mic_no;
-    microphone_averages_measured[mic_no] = average;
-  }
-  
-  have_identified_microphones = true;
-}
-
-bool all_non_zero(unsigned long (&values)[NUM_INPUTS]) {
-  for (size_t i = 0; i < NUM_INPUTS; i++) {
-    if (values[i] == 0) return false;
-  }
-  return true;
-}
-
-void process_tap(unsigned long (&times)[NUM_INPUTS]) {
-  double angle;
-  signed long t1 = times[0], t2 = times[1], t3 = times[2], t4 = times[3];
-  if (abs(t4 - t2) >= abs(t3 - t1)) {
-    angle = atan2(t4 - t2, t3 - t1);
-  } else {
-    angle = atan2(t3 - t1, t2 - t4) - M_PI_2;
-  }
-  
-  Serial.print("Tap: ");
-  for (size_t i = 0; i < NUM_INPUTS; i++) {
-      Serial.print(times[i]); Serial.print("\t");
-  }
-  Serial.print("\t");
-  Serial.println(angle);
-}
-
-void dump_buffer(InputBuffer &buff) {
-  for (size_t row = 0; row < INP_BUFF; row += NUM_INPUTS) {
-    for (size_t column = 0; column < NUM_INPUTS; column++) {
-      Serial.print(buff.data[row + column]);
-      Serial.print(',');
-    }
-    Serial.println();
-  }
 }
 
 void pio_TIOA0() { // Configure Ard pin 2 as output from TC0 channel A (copy of trigger event)
@@ -204,9 +62,9 @@ void adc_setup() {
   adc_disable_all_channel(ADC);
   adc_enable_interrupt(ADC, ADC_IER_RXBUFF);
 
-  ADC->ADC_RPR  = (uint32_t) buffers[0].data;      // DMA buffer
+  ADC->ADC_RPR  = (uint32_t) data;      // DMA buffer
   ADC->ADC_RCR  = INP_BUFF;
-  ADC->ADC_RNPR = (uint32_t) buffers[1].data;      // next DMA buffer
+  ADC->ADC_RNPR = (uint32_t) data;      // next DMA buffer
   ADC->ADC_RNCR = INP_BUFF;
   ADC->ADC_PTCR = 1;
 
@@ -220,48 +78,6 @@ void adc_setup() {
   adc_start(ADC);
 }
 
-void ADC_Handler(void) {
-  if ((adc_get_status(ADC) & ADC_ISR_RXBUFF) ==	ADC_ISR_RXBUFF) { // If the buffer is full
-    if (data_to_process==true){
-      Serial.println("Congestion. Last data has not been sent before next buffer was filled.");
-    }
-
-    data_start_time = data_end_time;
-    data_end_time = micros();
-
-    read_buffer_index = write_buffer_index;
-    write_buffer_index = (write_buffer_index + 1) % NUM_BUFFERS;
-
-    InputBuffer &buff = buffers[write_buffer_index];
-    buff.start_time = data_start_time;
-    buff.end_time   = data_end_time;
-
-    //set the next write
-    ADC->ADC_RNPR = (uint32_t)buff.data;
-    ADC->ADC_RNCR = INP_BUFF;
-
-    data_to_process = true;
-  }
-}
-
-////////////
-// Serial //
-////////////
-
-const unsigned long USB_BAUD_RATE = 1000000;
-const unsigned long BAUD_RATE = 115200;
-
-const uint8_t HOST_READY = 'r';
-const uint8_t HEADER_DATA_SIZE = 's';
-  // Sent at the start of the connection. Followed by:
-  // * the number of measurements per buffer (16 bits)
-  // * the number of inputs in the data (8 bits)
-
-const uint8_t BUFFER_START = 'b';
-  // Sent at the start of a buffer. Followed by:
-  // * the start time of the buffer (32 bits)
-  // * the end time of the buffer (32 bits)
-
 void write_uint16_t(uint16_t value) {
   // TODO: check the endianness
   for (int i = 0; i < 2; i++) {
@@ -269,55 +85,38 @@ void write_uint16_t(uint16_t value) {
   }
 }
 
-void write_unsigned_long(unsigned long value) {
-  for (int i = 0; i < 4; i++) {
-    SerialUSB.write((value >> (8 * i)) & 0xff);
-  }
-}
+void ADC_Handler(void) {
+  if ((adc_get_status(ADC) & ADC_ISR_RXBUFF) ==	ADC_ISR_RXBUFF) { // If the buffer is full
 
-void serial_init() {
-  SerialUSB.begin(USB_BAUD_RATE);
-  Serial.begin(BAUD_RATE);
-}
+    int maxs[NUM_INPUTS] = {0,0,0,0};
+    int mins[NUM_INPUTS] = {2048,2048,2048,2048};
 
-bool serial_connection_ready() {
-  if (SerialUSB.available() > 0) {
-    return SerialUSB.read() == HOST_READY;
-  } else {
-    return false;
-  }
-}
-
-void serial_send_size_header() {
-  // Tell the host what buffer sizes to expect
-  SerialUSB.write(HEADER_DATA_SIZE);
-  write_uint16_t(MEASUREMENTS_PER_BUFFER);
-  SerialUSB.write(NUM_INPUTS);
-}
-
-void serial_dump_buffer(InputBuffer &buff) {
-  SerialUSB.write(BUFFER_START);
-  write_unsigned_long(buff.start_time);
-  write_unsigned_long(buff.end_time);
-    for (int i = 0; i < NUM_INPUTS; i++){
-    SerialUSB.write(input_to_mic_number[i]);
-    Serial.print("MICS: ");
-    Serial.print(input_to_mic_number[i]);
-    Serial.print("\n");
-  }
-  for (int i = 0; i < INP_BUFF; i++) {
-    write_uint16_t(buff.data[i]);
-  }
-}
-
-bool check_buffer_for_tap(InputBuffer &buff) {
-  for (size_t i = 0; i < INP_BUFF; i += 1) {
-    int mic_no = input_to_mic_number[i%NUM_INPUTS];
-    if(abs(buff.data[i] - microphone_averages_measured[mic_no]) > THRESHOLD){
-      Serial.println(buff.data[i]);
-      have_identified_microphones=false;
-      return true;
+    for (int i = 0; i < INP_BUFF; i++) {
+      if (data[i] > maxs[i%NUM_INPUTS]){
+        maxs[i%NUM_INPUTS] = data[i];
+      }
+      if (data[i] < mins[i%NUM_INPUTS]){
+        mins[i%NUM_INPUTS] = data[i];
+      }
     }
+
+    boolean rangeFlag = false;
+
+    for (int m = 0; m < NUM_INPUTS; m++){
+      if(maxs[m] - mins[m] > 100 ){
+        rangeFlag = true;
+      }
+    }
+
+    if (rangeFlag == true){
+      for (int i = 0; i < INP_BUFF; i++) {
+        write_uint16_t(data[i]);
+      }
+    }
+    
+    //set the next write
+    ADC->ADC_RNPR = (uint32_t)data;
+    ADC->ADC_RNCR = INP_BUFF;
+
   }
-  return false;
 }
